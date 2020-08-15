ba5215727d95f1c4be3ef36417e6ff6622a8ad92
Luis Abellan <luis.abellan.dev@gmail.com>
2020-08-15 08:11:11 +0200

    ex1.py done

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,31 +1,62 @@
-index = {}
 
 
 def calculate(x, y, z):
     return 
 
 
-def get_indices_of_item_weights(weights, limit):
+
+
+def get_indices_of_item_weights(weights, length, limit):
     """
     YOUR CODE HERE
     """
     # Your code here
     
-    table = {}
-    input = (weights, limit)
+    # create dictionary
+    weights_dict = {}
+    result = []
+    # populate dictionary
+    for i in range(length):
+        
+        # populate dictionary
+        weights_dict[weights[i]] = i
 
-    for i in range(len(weights)):
-        if weights[]
+        value = limit - weights[i]
+        if value in weights_dict.keys():
 
-                 
+            # add indices to result
+            result.append(weights_dict[weights[i]])
+            result.append(weights_dict[value])
 
-    return table
+            # if there are only 2 items in the array
+            if result == [0,0]:
+                result[0] += 1 
 
+            # print(result)
+            return result
 
     
+  
+    
+       
+
+# # test 1    
+# weights_1 = [9]
+# get_indices_of_item_weights(weights_1, 1, 9)
+
+# # test 2
+# weights_2 = [4, 4]
+# answer_2 = get_indices_of_item_weights(weights_2, 2, 8)
+
+# test 3 
+# weights_3 = [4, 6, 10, 15, 16]
+# answer_3 = get_indices_of_item_weights(weights_3, 5, 21)
+# print(answer_3)
 
 
-    return ()
+# test 4
+# weights_4 = [12, 6, 7, 14, 19, 3, 0, 25, 40]
+# print(get_indices_of_item_weights(weights_4, 9, 7))
 
 
 
