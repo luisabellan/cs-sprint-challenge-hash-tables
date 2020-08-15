359e23ba58f3cb548ccdc0c32919c9f8f493cf05
Luis Abellan <luis.abellan.dev@gmail.com>
2020-08-15 05:50:46 +0200

    Update ex2.py

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -9,6 +9,47 @@ class Ticket:
         self.destination = destination
 
 
+
+
+tickets_dict = {}
+
+def reconstruct_trip(tickets, length):
+    """
+    YOUR CODE HERE
+    """
+    # Your code here
+    route = []
+    for ticket in tickets:
+        tickets_dict[ticket.source] = ticket.destination
+   
+    start = tickets_dict['NONE']
+    route.append(start)
+
+    next = None
+
+    for _ in range(1,length):
+        
+        next = tickets_dict[route[-1]]
+        route.append(next)
+
+    
+        
+    #print(f'route = {route}')
+    return route
+
+    
+""" 
+
+# test short case
+ticket_1 = Ticket("NONE", "PDX")
+ticket_2 = Ticket("PDX", "DCA")
+ticket_3 = Ticket("DCA", "NONE")
+
+tickets = [ticket_1, ticket_2, ticket_3]
+
+reconstruct_trip(tickets,len(tickets))
+
+# test long case
 ticket_1 = Ticket("PIT", "ORD")
 ticket_2 = Ticket("XNA", "SAP")
 ticket_3 = Ticket("SFO", "BHM")
@@ -23,53 +64,8 @@ ticket_10 = Ticket("BHM", "FLG")
 tickets = [ticket_1, ticket_2, ticket_3, ticket_4, ticket_5,
            ticket_6, ticket_7, ticket_8, ticket_9, ticket_10]
 
-tickets_dict = {}
+reconstruct_trip(tickets,len(tickets)) """
 
-def create_table():
-    for ticket in tickets:
-        tickets_dict[ticket.source] = ticket.destination
-
-create_table()
-
-print(tickets_dict)
-
-route = []
-
-# print(route)
-
-
-def reconstruct_trip(tickets, length):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
-   
-    start = tickets_dict['NONE']
-    route.append(start)
-
-    next = None
-
-    for _ in range(1,length-1):
-        
-        next = tickets_dict[route[-1]]
-        route.append(next)
-
-   
-
-                    
-
-
-        
-    print(f'route = {route}')
-  
-
-
-        
-        
-    
-   
-
-reconstruct_trip(tickets,10)
 
 
 
