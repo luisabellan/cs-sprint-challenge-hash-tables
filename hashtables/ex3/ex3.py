03a37c5ca57de64694b28dfa14ec252e02c589a0
Luis Abellan <luis.abellan.dev@gmail.com>
2020-08-15 20:40:49 +0200

    ex4.py done MVP reached
    ex3.py is not working

--- hashtables/ex3/ex3.py
+++ hashtables/ex3/ex3.py
@@ -4,14 +4,78 @@ def intersection(arrays):
     """
     # Your code here
 
-    return result
+
+        # # create 3 dictionaries
+        # dict1 = {
+        #     1 : none,
+        #     2 : none,
+        #     3 : none,
+        # }
+        # dict2 = {
+        #     1 : none,
+        #     4 : none,
+        #     5 : none,
+        # }
+        # dict3 = {
+        #     1 : none,
+        #     6 : none,
+        #     7 : none,
+        # }
+        # # loop dict 1
+        # if 
+    # dict1 = {}
+    # dict2 = {}
+    # dict3 = {}
+
+    # for array in arrays:
 
 
-if __name__ == "__main__":
-    arrays = []
+    # return result
+    # loop array of arrays
+    # find smaller array
+    
 
-    arrays.append(list(range(1000000, 2000000)) + [1, 2, 3])
-    arrays.append(list(range(2000000, 3000000)) + [1, 2, 3])
-    arrays.append(list(range(3000000, 4000000)) + [1, 2, 3])
+    dictionaries = {}
+    # for each array
+    for i in range(len(arrays)):
+        # for each
+        
+            dictionaries[i] = 
 
-    print(intersection(arrays))
+        print(dictionaries.values())
+
+    
+    print(f'dictionaries = {dictionaries}')
+   
+
+    # print(lengths)
+
+
+   
+
+
+
+# test code
+arrays = [[1,2,3],[1,4,5,3],[1,6,7,3]] 
+print(arrays)
+print(intersection(arrays))
+
+
+# if __name__ == "__main__":
+    # arrays = []
+
+    # arrays.append(list(range(1000000, 2000000)) + [1, 2, 3])
+    # arrays.append(list(range(2000000, 3000000)) + [1, 2, 3])
+    # arrays.append(list(range(3000000, 4000000)) + [1, 2, 3])
+
+    # test short case
+    # arrays = [[1,2,3],[1,4,5,3],[1,6,7,3]] 
+
+    # test long case
+
+    # arrays.append(list(range(1, 4)) + [1, 2, 3])
+    # arrays.append(list(range(1, 5)) + [1, 2, 3])
+    # arrays.append(list(range(1, 4)) + [1, 2, 3])
+
+    # print(arrays)
+    # print(intersection(arrays))
--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -4,8 +4,34 @@ def has_negatives(a):
     """
     # Your code here
 
-    return result
+    dict1 = {}
 
+    for i in range(len(a)):
+        dict1[i] = a[i] ;
+
+    #print(dict1)
+
+    result = []
+    # start_array = [-1, -2, 1, 2, 3, 4, -4]
+    # array = [-1, -2, -4]
+    array = []
+    for i in range(len(a)):
+        if a[i] < 0:
+            array.append(a[i])
+    #print(f'array is {array}')
+
+    for i in range(len(array)):
+        if (array[i] * (-1)) in a:
+            result.append((array[i] * (-1)))
+
+    return result
 
 if __name__ == "__main__":
     print(has_negatives([-1, -2, 1, 2, 3, 4, -4]))
+
+
+
+
+            
+
+
